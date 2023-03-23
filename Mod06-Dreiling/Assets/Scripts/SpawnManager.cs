a141d8f6d57c0c3017b7f9bfbad4ca3110dae396
DDreiling <124920637+DDreiling@users.noreply.github.com>
2023-03-23 13:48:22 -0500

    mod06B-Dreiling

---
+++ Mod06-Dreiling/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] animalPrefabs;
+    // public int animalIndex;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+           {
+            // Random gen animal index and spawn position 
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex]. transform.rotation);
+        }
+    }
+}
